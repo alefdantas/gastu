4b178e98f445262ab4ce849a92e2034700151d99
Daniel Macedo <enosmacedo@gmail.com>
2019-10-31 16:43:05 -0300

    atualizando projeto a forca

--- gastu/urls.py
+++ gastu/urls.py
@@ -5,6 +5,7 @@ app_name = 'gastu'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('<slug:slug>', views.post_detail, name='post_detail'),
     path('sobre-nos/', views.about, name='about'),
     path('contato/', views.contact, name='contact'),
 ]
