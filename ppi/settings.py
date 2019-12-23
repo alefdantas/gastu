8b5869e92a674ec3b99cf86a3563558dbfa3e85f
unknown <enosmacedo@gmail.com>
2019-12-23 20:25:04 -0300

    corrigindo o erro do projeto da pasta static

--- ppi/settings.py
+++ ppi/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_URL = '/gastu/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
